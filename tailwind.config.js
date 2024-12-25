fb9be48d6be3d920bfd6d52a23d88b4d8f7e95cd
Vladimir <vladimirrybakov123@gmail.com>
2024-12-25 19:45:26 +0000

    added the tailwind and linked it to the home and about pages so testing purposes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './slpp/templates/**/*.html',
+    './slpp/slpp/**/*.py',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
